6dc72bdb474a80e45a09761a9098e26225b8885e
Jyotirmayee Biswal <159261131+jyotirmayeebiswal@users.noreply.github.com>
2024-12-26 00:03:08 +0530

    Add files via upload

---
+++ src/main/java/net/javaguides/sms/StudentManagementSystemApplication.java
@@ -0,0 +1,23 @@
+package net.javaguides.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import net.javaguides.sms.repository.StudentRepository;
+import net.javaguides.sms.entity.Student;
+
+@SpringBootApplication
+public class StudentManagementSystemApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystemApplication.class, args);
+	}
+	
+	@Autowired
+	private StudentRepository studentRepository;
+	@Override
+	public void run(String... args) throws Exception{
+	}
+
+}
